adab9b4021a76dfb42d7a88d6ce7dd5eb528d68c
Zeynep BARAN <zbaran@outlook.com.tr>
2023-10-08 22:01:57 +0300

    layers complete

---
+++ zbilisim.Model/Context/zbilisimContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using zbilisim.Model.Entities;
+using zbilisim.Model.Maps;
+
+namespace zbilisim.Model.Context
+{
+    public class zbilisimContext:DbContext
+    {
+        public zbilisimContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new FormMap());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<FormTable> FormTables { get; set; }
+
+    }
+}
---
+++ zbilisim.Model/Maps/FormMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using zbilisim.Core.Maps;
+using zbilisim.Model.Entities;
+
+namespace zbilisim.Model.Maps
+{
+    public class FormMap:CoreMap<FormTable>
+    {
+        public override void Configure(EntityTypeBuilder<FormTable> builder)
+        {
+            builder.Property(x => x.NameSurname)
+                .HasMaxLength(250)
+                .IsRequired(true);
+
+            builder.Property(x => x.Email)
+                .HasMaxLength(250)
+                .IsRequired(true);
+
+            builder.Property(x => x.Message)
+                .HasMaxLength(500)
+                .IsRequired(true);
+        }
+    }
+}
